fd78f0ec2093e1aab85220a325e3733002e99a4e
adityaslyf <aditya.varshneymail@gmail.com>
2024-06-25 00:06:37 +0530

    added cards layout

--- app/components/Card/page.tsx
+++ app/components/Card/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Card1 from "../../../public/card1.png";
 
-const page = () => {
-  return (
+const Page = () => {
+  const cards = Array(6).fill(
     <div
-      className="w-[384px] h-[384px] mb-96 rounded-[1.875rem]"
+      className="w-[384px] h-[384px]  rounded-[1.875rem] flex-shrink-0"
       style={{
         backgroundImage: `url("/card1.png")`,
         backgroundSize: "cover",
@@ -12,11 +11,21 @@ const page = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="p-6  opacity-80 text-white text-xl font-medium font-['Instrument Sans'] leading-7 rounded-[1.8rem]">
-          You have a nice nft that you want to share among your friends.
-                </div>
+      <div className="p-6 opacity-80 text-white text-xl font-medium font-['Instrument Sans'] leading-7 rounded-[1.8rem]">
+        You have a nice NFT that you want to share among your friends.
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {cards.map((card, index) => (
+          <div key={index}>{card}</div>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
