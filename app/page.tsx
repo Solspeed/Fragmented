027185b75cae29b19ac6c1ee27bfbe2f433a4e0d
adityaslyf <aditya.varshneymail@gmail.com>
2024-06-25 20:01:14 +0530

    comment out working section

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       {/* <Navbar /> */}
       <Main />  
       <Description />
-      {/* <Working /> */}
+      <Working />
       <UseCase />
     </main>
   );
